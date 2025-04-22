d63eaeabb276f0101083c97d849530f66ae34ec7
james.macdonald <james.macdonald@heanet.ie>
2025-04-22 10:39:35 +0100

    use flake utils

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
     pyproject-nix = {
       url = "github:pyproject-nix/pyproject.nix";
@@ -23,119 +24,96 @@
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      uv2nix,
-      pyproject-nix,
-      pyproject-build-systems,
-      ...
-    }:
-    let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    uv2nix,
+    pyproject-nix,
+    pyproject-build-systems,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
       inherit (nixpkgs) lib;
-      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+      workspace = uv2nix.lib.workspace.loadWorkspace {workspaceRoot = ./.;};
 
       # Create package overlay from workspace.
       overlay = workspace.mkPyprojectOverlay {
-        sourcePreference = "wheel"; # or sourcePreference = "sdist";
-        # Optionally customise PEP 508 environment
-        # environ = {
-        #   platform_release = "5.10.65";
-        # };
+        sourcePreference = "wheel";
       };
 
       pyprojectOverrides = _final: _prev: {};
 
-      # This example is only using x86_64-linux
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-
-      # Use Python 3.13 from nixpkgs
+      pkgs = nixpkgs.legacyPackages.${system};
       python = pkgs.python313;
 
       # Construct package set
       pythonSet =
-        # Use base package set from pyproject.nix builders
         (pkgs.callPackage pyproject-nix.build.packages {
           inherit python;
         }).overrideScope
-          (
+        (
+          lib.composeManyExtensions [
+            pyproject-build-systems.overlays.default
+            overlay
+            pyprojectOverrides
+          ]
+        );
+    in {
+      formatter = nixpkgs.legacyPackages.${system}.alejandra;
+      packages = {
+        default = pythonSet.mkVirtualEnv "jtec-env" workspace.deps.default;
+
+        dockerImage = pkgs.dockerTools.buildLayeredImage {
+          name = "jtec";
+          tag = "latest";
+          created = "now";
+          contents = [];
+          config = {
+            Cmd = [
+              "${self.packages.${system}.default}/bin/jtec"
+            ];
+            Env = [];
+          };
+        };
+      };
+
+      apps.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/jtec";
+      };
+
+      devShells = {
+        uv2nix = let
+          editableOverlay = workspace.mkEditablePyprojectOverlay {
+            root = "$REPO_ROOT";
+          };
+
+          editablePythonSet = pythonSet.overrideScope (
             lib.composeManyExtensions [
-              pyproject-build-systems.overlays.default
-              overlay
-              pyprojectOverrides
+              editableOverlay
+
+              (final: prev: {
+                jtec = prev.jtec.overrideAttrs (old: {
+                  src = lib.fileset.toSource {
+                    root = old.src;
+                    fileset = lib.fileset.unions [
+                      (old.src + "/pyproject.toml")
+                      (old.src + "/README.md")
+                      (old.src + "/src/jtec/__init__.py")
+                    ];
+                  };
+                  nativeBuildInputs =
+                    old.nativeBuildInputs
+                    ++ final.resolveBuildSystem {
+                      editables = [];
+                    };
+                });
+              })
             ]
           );
-
-    in
-    {
-      # Package a virtual environment as our main application.
-      #
-      # Enable no optional dependencies for production build.
-      packages.x86_64-linux.default = pythonSet.mkVirtualEnv "jtec-env" workspace.deps.default;
-
-      # Make jtec runnable with `nix run`
-      apps.x86_64-linux = {
-        default = {
-          type = "app";
-          program = "${self.packages.x86_64-linux.default}/bin/jtec";
-        };
-      };
-
-      dockerImage = pkgs.dockerTools.buildLayeredImage {
-        name = "jtec";
-        contents = [ ];
-        config = {
-          Cmd = [
-            "${self.packages.x86_64-linux.default}/bin/jtec"
-          ];
-          Env = [];
-        };
-      };
-
-      devShells.x86_64-linux = {
-
-        uv2nix =
-          let
-            # Create an overlay enabling editable mode for all local dependencies.
-            editableOverlay = workspace.mkEditablePyprojectOverlay {
-              # Use environment variable
-              root = "$REPO_ROOT";
-              # Optional: Only enable editable for these packages
-              # members = [ "jtec" ];
-            };
-
-            # Override previous set with our overrideable overlay.
-            editablePythonSet = pythonSet.overrideScope (
-              lib.composeManyExtensions [
-                editableOverlay
-
-                # Apply fixups for building an editable package of your workspace packages
-                (final: prev: {
-                  jtec = prev.jtec.overrideAttrs (old: {
-                    # It's a good idea to filter the sources going into an editable build
-                    # so the editable package doesn't have to be rebuilt on every change.
-                    src = lib.fileset.toSource {
-                      root = old.src;
-                      fileset = lib.fileset.unions [
-                        (old.src + "/pyproject.toml")
-                        (old.src + "/README.md")
-                        (old.src + "/src/jtec/__init__.py")
-                      ];
-                    };
-                    nativeBuildInputs =
-                      old.nativeBuildInputs
-                      ++ final.resolveBuildSystem {
-                        editables = [ ];
-                      };
-                  });
-
-                })
-              ]
-            );
-            virtualenv = editablePythonSet.mkVirtualEnv "jtec-dev-env" workspace.deps.all;
-
-          in
+          virtualenv = editablePythonSet.mkVirtualEnv "jtec-dev-env" workspace.deps.all;
+        in
           pkgs.mkShell {
             packages = [
               virtualenv
@@ -143,24 +121,16 @@
             ];
 
             env = {
-              # Don't create venv using uv
               UV_NO_SYNC = "1";
-
-              # Force uv to use Python interpreter from venv
               UV_PYTHON = "${virtualenv}/bin/python";
-
-              # Prevent uv from downloading managed Python's
               UV_PYTHON_DOWNLOADS = "never";
             };
 
             shellHook = ''
-              # Undo dependency propagation by nixpkgs.
               unset PYTHONPATH
-
-              # Get repository root using git. This is expanded at runtime by the editable `.pth` machinery.
               export REPO_ROOT=$(git rev-parse --show-toplevel)
             '';
           };
       };
-    };
+    });
 }
